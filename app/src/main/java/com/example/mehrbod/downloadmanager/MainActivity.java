7bfe9e15862a529d4cdeee99ca1ae70189f76cb9
Mehrbod Khiabani <mehrbod.kh@gmail.com>
2017-07-22 09:28:32 +0430

    CardView added with extended name and ProgressBar

--- app/src/main/java/com/example/mehrbod/downloadmanager/MainActivity.java
+++ app/src/main/java/com/example/mehrbod/downloadmanager/MainActivity.java
@@ -6,7 +6,13 @@ import android.app.DownloadManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.webkit.URLUtil;
+import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.example.mehrbod.downloadmanager.Downloader.DownloadHelper;
 
@@ -16,6 +22,10 @@ import java.util.concurrent.Executors;
 public class MainActivity extends AppCompatActivity {
 
     private ProgressBar progressBar;
+    private EditText urlEditText;
+    private TextView nameTextView;
+
+    private DownloadHelper downloadHelper = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,21 +36,49 @@ public class MainActivity extends AppCompatActivity {
                 new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,
                         Manifest.permission.INTERNET, Manifest.permission.ACCESS_NETWORK_STATE}, 1);
 
-        String url = "http://dl.pop-music.ir/music/1395/Tir/Reza%20Yazdani%20-%20Az%20Tanhayi%20Mitarsam.mp3";
+
+
 
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
-        DownloadHelper downloadHelper = new DownloadHelper(
+        urlEditText = (EditText) findViewById(R.id.mainActivityUrlEditText);
+        nameTextView = (TextView) findViewById(R.id.mainActivityNameTextView);
+
+        urlEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                nameTextView.setText(URLUtil.guessFileName(
+                        urlEditText.getText().toString(), null, null
+                ));
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+    }
+
+    public void onDownloadButtonClickListener(View view) {
+        String url = urlEditText.getText().toString();
+
+        downloadHelper = new DownloadHelper(
                 this,
                 url ,
                 DownloadManager.Request.NETWORK_WIFI
         );
-
 
         downloadHelper.prepareDownload();
         downloadHelper.startDownload();
 
         ExecutorService executorService = Executors.newCachedThreadPool();
         executorService.execute(new Progress(this, progressBar, downloadHelper));
+
     }
 }
 
